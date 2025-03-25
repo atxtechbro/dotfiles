70550a221c27c0af0d085c7e9f69b1f8f23f3fd7
Morgan Joyce <morganj2k@gmail.com>
2025-03-24 20:08:36 -0500

    refactor(nvim): minimize config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,27 +1,8 @@
--- ~/.config/nvim/init.lua
 -- Complete Neovim configuration in a single file
-
--- IMPORTANT: Set leader key first, before anything else loads
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
 
 -- Basic settings
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.smartindent = true
-vim.opt.termguicolors = true
-vim.opt.mouse = 'a'
-vim.opt.clipboard = 'unnamedplus' -- System clipboard integration
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
-vim.opt.completeopt = 'menuone,noselect'
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = 'yes' -- Reserve space for diagnostic icons
 
 -- Bootstrap Packer if not installed
 local ensure_packer = function()
@@ -54,30 +35,15 @@ require('packer').startup(function(use)
   use 'neovim/nvim-lspconfig'           -- LSP configuration
   use 'williamboman/mason.nvim'         -- Package manager for LSP servers
   use 'williamboman/mason-lspconfig.nvim' -- Integration with lspconfig
-  
-  -- Completion plugins
-  use 'hrsh7th/nvim-cmp'                -- Completion engine
-  use 'hrsh7th/cmp-nvim-lsp'            -- LSP source for nvim-cmp
-  use 'hrsh7th/cmp-buffer'              -- Buffer source for nvim-cmp
-  use 'hrsh7th/cmp-path'                -- Path source for nvim-cmp
-  use 'L3MON4D3/LuaSnip'                -- Snippet engine
-  use 'saadparwaiz1/cmp_luasnip'        -- Snippets source for nvim-cmp
-  
-  -- Treesitter for better syntax highlighting
   use {
     'nvim-treesitter/nvim-treesitter',
     run = ':TSUpdate'
   }
-  
   -- Telescope fuzzy finder
   use {
     'nvim-telescope/telescope.nvim',
     requires = {{'nvim-lua/plenary.nvim'}}
   }
-  
-  -- File icons (for Telescope)
-  use 'nvim-tree/nvim-web-devicons'
-  
   -- Automatically set up configuration after cloning packer.nvim
   if packer_bootstrap then
     require('packer').sync()
@@ -86,45 +52,6 @@ end)
 
 -- Keymaps
 local opts = { noremap = true, silent = true }
-
--- Better window navigation
-vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
-vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
-vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
-vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
-
--- Resize with arrows
-vim.keymap.set('n', '<C-Up>', ':resize -2<CR>', opts)
-vim.keymap.set('n', '<C-Down>', ':resize +2<CR>', opts)
-vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', opts)
-vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', opts)
-
--- Stay in indent mode
-vim.keymap.set('v', '<', '<gv', opts)
-vim.keymap.set('v', '>', '>gv', opts)
-
--- Move text up and down
-vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", opts)
-vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", opts)
-
--- Keep yanked text when pasting over selection
-vim.keymap.set('v', 'p', '"_dP', opts)
-
--- Clear search highlights
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>', opts)
-
--- Quick save
-vim.keymap.set('n', '<leader>w', ':write<CR>', opts)
-
--- Buffer navigation
-vim.keymap.set('n', '<S-l>', ':bnext<CR>', opts)
-vim.keymap.set('n', '<S-h>', ':bprevious<CR>', opts)
-
--- Telescope mappings using direct key mappings, not leader
-vim.keymap.set('n', '<F5>', '<cmd>Telescope find_files<cr>', opts)
-vim.keymap.set('n', '<F6>', '<cmd>Telescope live_grep<cr>', opts)
-vim.keymap.set('n', '<F7>', '<cmd>Telescope buffers<cr>', opts)
-vim.keymap.set('n', '<F8>', '<cmd>Telescope help_tags<cr>', opts)
 
 -- Also add the leader-based keymaps (for when leader works correctly)
 vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>', opts)
@@ -221,38 +148,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
   desc = 'LSP actions',
   callback = function(event)
     local opts = {buffer = event.buf}
-    
-    -- Hover documentation
     vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-    
-    -- Code navigation
     vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-    
-    -- Workspace management
-    vim.keymap.set('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>', opts)
-    vim.keymap.set('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>', opts)
-    vim.keymap.set('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>', opts)
-    
-    -- Code actions
-    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-    vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-    
-    -- Alternative mappings using <leader>
-    vim.keymap.set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-    vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-    vim.keymap.set('n', '<leader>f', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-    
-    -- Diagnostics
-    vim.keymap.set('n', '<leader>d', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
-    vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
-    vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
-    vim.keymap.set('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<cr>', opts)
+    vim.keymap.set('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
   end,
 })
 
