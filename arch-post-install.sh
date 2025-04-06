4e7ad4dbd5cdd7e4e988d507d8a300b0c37cb4f2
Morgan Joyce <morganj2k@gmail.com>
2025-04-05 19:50:48 -0500

    fix: remove unused RED color variable from post-install script

--- arch-post-install.sh
+++ arch-post-install.sh
@@ -7,7 +7,6 @@ set -e  # Exit on error
 # Colors for output
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
-RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 echo -e "${GREEN}Starting post-installation setup...${NC}"
