a0d129ba651ea8ee913dd5b21049f2be805b98f7
Morgan Joyce <morganj2k@gmail.com>
2025-05-15 14:10:46 -0500

    WIP: Progress on Amazon Q and clipboard functionality. Closes #178

--- .bash_aliases.d/clipboard.sh
+++ .bash_aliases.d/clipboard.sh
@@ -7,3 +7,10 @@ alias clip-cmd='PREV_CMD=$(fc -ln -2 -2 | sed "s/^ *//"); (echo "Command: $PREV_
 # Quick clipboard access
 alias clip="xclip -selection clipboard"
 
+# Copy Amazon Q security recommendations to clipboard
+# Works with qtrust alias in q-cli.sh for a complete security workflow
+qsafe() {
+    # Format as a single command with all tools on one line
+    echo "/tools untrust fs_write execute_bash use_aws github___create_issue github___add_issue_comment github___push_files github___create_or_update_file github___create_repository gitlab___push_files gitlab___create_or_update_file git___git_commit git___git_add" | xclip -selection clipboard
+}
+
