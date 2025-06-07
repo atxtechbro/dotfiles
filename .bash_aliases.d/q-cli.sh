e29c4d08f5c8dd57e0d24443f79cf3cf50910a10
Morgan Joyce <morganj2k@gmail.com>
2025-06-07 11:27:00 -0500

    feat(q-cli): add qsafe_output for Amazon Q context hooks
    - Add qsafe_output function that outputs untrust command to stdout
    - Enables automatic security tool untrusting via Amazon Q context hooks
    - Complements existing qsafe function that copies to clipboard

--- .bash_aliases.d/q-cli.sh
+++ .bash_aliases.d/q-cli.sh
@@ -71,3 +71,41 @@ qsafe() {
     | xclip -selection clipboard
 }
 
+# Version of qsafe that outputs to stdout for Amazon Q context hooks
+# This allows the untrust command to be automatically injected into Q sessions
+qsafe_output() {
+    echo "/tools untrust fs_write execute_bash use_aws report_issue\
+    filesystem___create_directory\
+    filesystem___move_file\
+    github___create_issue\
+    github___create_repository\
+    github___delete_file\
+    github___push_files\
+    github___create_branch\
+    github___merge_pull_request\
+    github___update_issue\
+    github___update_pull_request\
+    github___create_pull_request\
+    github___fork_repository\
+    github___add_issue_comment\
+    github___create_and_submit_pull_request_review\
+    github___submit_pending_pull_request_review\
+    github___add_pull_request_review_comment_to_pending_review\
+    github___create_pending_pull_request_review\
+    github___delete_pending_pull_request_review\
+    github___assign_copilot_to_issue\
+    github___request_copilot_review\
+    github___mark_all_notifications_read\
+    github___dismiss_notification\
+    github___manage_notification_subscription\
+    github___manage_repository_notification_subscription\
+    github___update_pull_request_branch\
+    atlassian___jira_delete_issue \
+    atlassian___confluence_delete_page\
+    atlassian___jira_remove_issue_link\
+    atlassian___confluence_add_label \
+    atlassian___jira_batch_create_issues\
+    atlassian___jira_create_sprint\
+    atlassian___jira_link_to_epic"
+}
+
